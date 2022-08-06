3415619fa449f533194175ba82807c98409114a6
Jason_Yang_hz <596875904@qq.com>
2022-08-06 22:38:17 +0800

    b

--- src/seg.py
+++ src/seg.py
@@ -3,7 +3,11 @@ import os, sys
 def seg(args):
     input_file = os.path.join(args.datadir, "images")
     save_folder = os.path.join(args.datadir, "segmentation")
+    if os.path.exist(save_folder) == True:
+        print("segmentation already finished!")
+        return
     os.makedirs(save_folder, exist_ok=True)
     config_fpath = "mseg-semantic/mseg_semantic/config/test/default_config_360_ms.yaml"
     os.system("python -u mseg-semantic/mseg_semantic/tool/universal_demo.py --config=%s --file_save 2 model_name mseg-3m model_path mseg-3m.pth input_file %s save_folder %s" %
               (config_fpath, input_file, save_folder))
+    print("segmentation finished!")
